7995a32d068b82917030fd467937486d3d89c9fe
vandana <vandana@akonect.com>
2018-03-21 17:06:03 +0530

    Modified login selector to look for link

--- pages/SignUpAndLogin.js
+++ pages/SignUpAndLogin.js
@@ -5,7 +5,7 @@ var signUpAndloginTrell = {
 
             console.log('trying to login');
     
-            $(".global-header-section-button").click();
+            $('a[href^="/login"]').click();
             browser.sleep(5000);
             $("#user").sendKeys("uitesterSix@yopmail.com");
             $("#password").sendKeys("uitest@123");
@@ -16,7 +16,7 @@ var signUpAndloginTrell = {
     },
     signUp: function(){
 
-
+        
 
     }
 
