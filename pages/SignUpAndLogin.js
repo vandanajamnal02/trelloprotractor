b2f504399d6b5503efd68c168106f65da532e99d
vandana <vandana@akonect.com>
2018-03-21 16:56:18 +0530

    Changed element selector to $

--- pages/SignUpAndLogin.js
+++ pages/SignUpAndLogin.js
@@ -5,18 +5,18 @@ var signUpAndloginTrell = {
 
             console.log('trying to login');
     
-            element(by.xpath("//a[@class='global-header-section-button']")).click();
+            $(".global-header-section-button").click();
             browser.sleep(5000);
-            element(by.id("user")).sendKeys("uitesterSix@yopmail.com");
-            element(by.id("password")).sendKeys("uitest@123");
-            element(by.id("login")).click();
+            $("#user").sendKeys("uitesterSix@yopmail.com");
+            $("#password").sendKeys("uitest@123");
+            $("#login").click();
             browser.sleep(5000);
       
 
     },
     signUp: function(){
 
-        
+
 
     }
 
